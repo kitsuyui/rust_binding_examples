9c0175ef7e80f360c62939ea86d3f5662ce1af35
kitsuyui <kitsuyui@kitsuyui.com>
2022-07-31 17:10:29 +0900

    Fix clippy warnings

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -3,7 +3,7 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 fn my_calc(a: i64, b: i64, c: i64) -> PyResult<String> {
-    Ok(amazing_calc::my_calc(a, b, c).to_string())
+    Ok(amazing_calc::my_calc(a, b, c))
 }
 
 
